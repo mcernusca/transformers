8889a6fdf5c4d20f78d646a0eed00ccf9617655a
ydshieh <ydshieh@users.noreply.github.com>
2022-10-31 20:47:48 +0100

    update

--- tests/models/canine/test_modeling_canine.py
+++ tests/models/canine/test_modeling_canine.py
@@ -48,7 +48,7 @@ class CanineModelTester:
         use_input_mask=True,
         use_token_type_ids=True,
         use_labels=True,
-        vocab_size=99,
+        vocab_size=100000,
         hidden_size=32,
         num_hidden_layers=5,
         num_attention_heads=4,
